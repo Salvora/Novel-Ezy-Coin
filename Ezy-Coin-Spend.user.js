ff78f8e1e50f37675d251473a6ad74084520ce40
Salvora <6360098+Salvora@users.noreply.github.com>
2025-01-09 03:17:15 +0300

    increment version

--- Ezy-Coin-Spend.user.js
+++ Ezy-Coin-Spend.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Novel-Ezy-Coin
 // @namespace   https://github.com/Salvora
-// @version     1.8.1
+// @version     1.8.2
 // @grant       GM_addStyle
 // @grant       GM_getResourceText
 // @grant       GM_setValue
